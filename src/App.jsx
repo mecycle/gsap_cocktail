e0abbde4a5188b23a404e484f2001c7b0c2d4f99
Yuxin Zhang <53304036+mecycle@users.noreply.github.com>
2025-07-24 14:42:40 +0930

    add nav & imgs folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import gsap from 'gsap';
+import Navbar from './component/Navbar';
+
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='text-3xl text-indigo-300'>Hello, World!</h1>
-    </div>
+    <main className='h-[200vh]'>
+      <Navbar />
+    </main>
   );
 }
 
